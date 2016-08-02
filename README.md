cdddacff16070223f14009ea9ffd5c6a8868f8db
大貘 <weihua.lwh@alibaba-inc.com>
2016-08-02 10:42:37 +0800

    change the project files

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # SassMagic
 
-[![Codewake](https://www.codewake.com/badges/ask_question_flat_square.svg)](https://www.codewake.com/p/sass-resources)
+![npm version](http://img.shields.io/npm/v/SassMagic.svg)
+![ci status](https://api.travis-ci.org/amfe/SassMagic.svg?branch=master)
 
 SassMagic is a collection of Sass mixins and functions to ease and improve our implementations of common style-code patterns.
 
 Collection best of Sass Mixins and Function.If you have seen good and meaningful Mixins or Functions, please send us your code.Let more students share your results.
 
 
+## License
 
-© [w3cplus](http://www.w3cplus.com).作者:大漠。QQ讨论群：78142855
+(The MIT License)
+
+Copyright (©) 2016 [W3cplus](http://www.w3cplus.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp =  require('gulp');
+
+var sass = require('gulp-ruby-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var px2rem = require('gulp-px3rem');
+
+var eslint = require('gulp-eslint');
+
+var htmlone = require('gulp-htmlone');
+
+var rename = require('gulp-rename');
+var del = require('del');
+
+var paths = {
+  src: './src',
+  build: './build'
+};
+
+// css
+gulp.task('sass', function () {
+  return sass(paths.src + '/**/*.scss')
+    .on('error', sass.logError)
+    .pipe(gulp.dest(paths.build + '/css/'));
+});
+
+gulp.task('css', ['sass'], function () {
+  return gulp.src(paths.src + '/**/*.css', {base: paths.src})
+    .pipe(px2rem({
+      threeVersion: false,
+      // XXX: 以下两项根据项目实际情况做修改
+      remUnit: 75,
+      baseDpr: 2,
+      forcePxComment: 'px',
+      keepComment: 'no'
+    }))
+    // px3rem 默认输出的文件带有 .debug 后缀
+    .pipe(rename(function (path) {
+      path.basename = path.basename.replace('.debug', '');
+    }))
+    .pipe(postcss([
+      // 自动添加厂商前缀
+      autoprefixer({
+        browsers: ['> 1%', 'last 2 versions']
+      })
+    ]))
+    .pipe(gulp.dest(paths.src));
+});
+
+// js
+gulp.task('eslint', function () {
+  return gulp.src(paths.src + '/js/**/*.js', {base: paths.src})
+    .pipe(eslint())
+    .pipe(eslint.format())
+    // 如果 eslint 有 error 则报错，不执行后续操作
+    .pipe(eslint.failAfterError());
+});
+
+// combo
+// 1. htmlone 集成了 uglify-js 和 ycssmin 插件，所以不需要额外的 minify 方案
+gulp.task('htmlone', ['css', 'eslint'], function () {
+  // 先保存一个未压缩版
+  gulp.src(paths.src + '/**/*.html', {base: paths.src})
+    .pipe(htmlone({
+      cssminify: false,
+      jsminify: false
+    }))
+    .pipe(rename({
+      suffix: '.debug'
+    }))
+    .pipe(gulp.dest(paths.build));
+
+  // 再保存一个压缩版
+  return gulp.src(paths.src + '/**/*.html', {base: paths.src})
+    .pipe(htmlone({
+      cssminify: true,
+      jsminify: true
+    }))
+    .pipe(gulp.dest(paths.build));
+});
+
+// clean
+gulp.task('clean', function () {
+  del(paths.build);
+});
+
+gulp.task('watch', ['default'], function () {
+  //gulp.watch([paths.src + '/js/*.js'], ['eslint']);
+  gulp.watch([paths.src + '/**/*.scss'], ['css']);
+});
+
+gulp.task('build', ['clean', 'sass', 'css', 'eslint', 'htmlone']);
+gulp.task('default', ['build']);
+
